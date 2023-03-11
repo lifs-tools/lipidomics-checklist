2b7631763360b5d50bd9a2734463105f9f253f10
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2023-03-11 09:39:46 +0100

    repaired pdf creation

--- create_report.py
+++ create_report.py
@@ -19,6 +19,7 @@ def fill_report_fields(mycursor, table_prefix, uid, entry_id, titles, report_fie
     for page in result["pages"]:
         for field in page["content"]:
             field_name = field["name"]
+            visible[field_name] = True
             
             field_map[field_name] = field
             if field["type"] in {"select", "multiple"} and "choice" in field:
@@ -54,8 +55,7 @@ def fill_report_fields(mycursor, table_prefix, uid, entry_id, titles, report_fie
                     if value[0] == "'" and value[-1] == "'":
                         value = value[1 : -1]
                     
-                    else :
-                        # check if value is a number
+                    else:
                         try:
                             value = float(value)
                         except:
@@ -66,15 +66,19 @@ def fill_report_fields(mycursor, table_prefix, uid, entry_id, titles, report_fie
             conditions[field_name] = condition
     
     
-    for field_name in conditions:
-        visible[field_name] = False
-        for condition_and in conditions[field_name]:
-            condition_met = True
-            for single_condition in condition_and:
-                key, operator, value = single_condition
-                conditional_field = choice_to_field[key]
-                condition_met &= (conditional_field in visible and visible[conditional_field]) and (operator == "=" and field_map[key]["value"] == value) or (operator == "~" and field_map[key]["value"] != value)
-            visible[field_name] |= condition_met
+    
+    for page in result["pages"]:
+        for field in page["content"]:
+            if "condition" not in field or len(field["condition"]) == 0: continue
+            field_name = field["name"]
+            visible[field_name] = False
+            for condition_and in conditions[field_name]:
+                condition_met = True
+                for single_condition in condition_and:
+                    key, operator, value = single_condition
+                    conditional_field = choice_to_field[key]
+                    condition_met &= (conditional_field in visible and visible[conditional_field]) and (operator == "=" and field_map[key]["value"] == value) or (operator == "~" and field_map[key]["value"] != value)
+                visible[field_name] |= condition_met
     
     
     for page in result["pages"]:
@@ -88,15 +92,17 @@ def fill_report_fields(mycursor, table_prefix, uid, entry_id, titles, report_fie
             if field["name"] in visible and not visible[field["name"]]: continue
         
             if field["type"] == "text":
+                #print(field["label"], field["value"])
+            
                 if field["label"][:5].lower() == "other":
                     if field["label"][6:].lower() in values:
-                        values[field["label"][6:].lower()] = field["value"]
+                        values[field["label"][6:].lower()][-1] = field["value"]
                 else:
-                    values[field["label"].lower()] = field["value"]
+                    values[field["label"].lower()] = [field["value"]]
                     report_fields[-1].append([field["label"], ""])
                 
             elif field["type"] == "number":
-                values[field["label"].lower()] = str(field["value"])
+                values[field["label"].lower()] = [str(field["value"])]
                 report_fields[-1].append([field["label"], ""])
                 
             elif field["type"] in {"select", "multiple"}:
@@ -104,7 +110,7 @@ def fill_report_fields(mycursor, table_prefix, uid, entry_id, titles, report_fie
                 for choice in field["choice"]:
                     if choice["value"] == 1:
                         choice_values.append(choice["label"])
-                values[field["label"].lower()] = "\n".join(choice_values)
+                values[field["label"].lower()] = choice_values
                 report_fields[-1].append([field["label"], ""])
     
     
@@ -112,120 +118,8 @@ def fill_report_fields(mycursor, table_prefix, uid, entry_id, titles, report_fie
         for i in range(len(report_fields[-1])):
             key = report_fields[-1][i][0].lower()
             if key in values:
-                report_fields[-1][i][1] = values[key]
+                report_fields[-1][i][1] = ", ".join(values[key])
     
-    return
-    
-    sql = "SELECT p.post_content AS form FROM %sposts AS p INNER JOIN %swpforms_entries AS e ON p.id = e.form_id WHERE e.user_id = %i and e.entry_id = %i;" % (table_prefix, table_prefix, uid, entry_id)
-    mycursor.execute(sql)
-    form_content = json.loads(mycursor.fetchone()["form"])["fields"]
-    
-    
-    sql = "SELECT fields FROM %swpforms_entries WHERE user_id = %i and entry_id = %i;" % (table_prefix, uid, entry_id)
-    mycursor.execute(sql)
-    db_fields = json.loads(mycursor.fetchone()["fields"])
-    field_values = {k: set(row["value"].split("\n")) for k, row in db_fields.items()}
-    
-    
-    field_ids = {}
-    conditions = {}
-    possible_values = {}
-
-    
-    for i, (k, v) in enumerate(form_content.items()):
-        if "title" in v:
-            titles.append(v["title"])
-            report_fields.append([])
-
-        elif "label" in v:
-            if len(report_fields) == 0:
-                report_fields.append([])
-                
-            if "type" in v and v["type"] != "hidden":
-                report_fields[-1].append(v["label"])
-            
-            field_ids[v["label"]] = k
-            
-            if "conditional_logic" in v and v["conditional_logic"] == '1':
-                conditions[k] = v["conditionals"]
-                if type(conditions[k]) == dict:
-                    conditions[k] = [vv for kk, vv in conditions[k].items()]
-            
-            if "type" in v and v["type"] in {"checkbox", "select"}:
-                possible_values[k] = {}
-                for ci, cv in v["choices"].items():
-                    possible_values[k][ci] = cv["label"]
-                    
-    
-    # 'other' substitution
-    for field_name, field_id in field_ids.items():
-        if field_name[:5].lower() == "other" and field_id in conditions and field_id in field_values:
-            other_value = field_values[field_id]
-            ref_id = conditions[field_id][0][0]["field"]
-            if ref_id not in field_values: continue
-        
-            cnt = sum([f[:5].lower() == "other" for f in field_values[ref_id]])
-            if len(other_value) == 1 and cnt == 1:
-                other_value = "".join(other_value)
-                field_values[ref_id] = {f if f[:5].lower() != "other" else other_value for f in field_values[ref_id]}
-                
-    # checking for conditionals
-    for report_section in report_fields:
-        new_section = []
-        for report_field in report_section:
-            if report_field[:5].lower() == "other" or report_field not in field_ids: continue
-        
-            field_id = field_ids[report_field]
-            field_value = field_values[field_id] if field_id in field_values else {""}
-            
-            
-            if field_id not in conditions:
-                new_section.append([report_field, ", ".join(sorted(list(field_value)))])
-                
-            else:
-                condition_satisfied = False
-                for or_cond in conditions[field_id]:
-                    if condition_satisfied: break
-                
-                    c = True
-                    for and_cond in or_cond:
-                        cfield_id = and_cond["field"]
-                        if cfield_id not in field_values:
-                            c = False
-                            break
-                        
-                        value_id = and_cond["value"]
-                        c_op = and_cond["operator"]
-                        
-                        if not str.isnumeric(value_id):
-                            c_value = value_id
-                        else:
-                            if cfield_id not in possible_values:
-                                c = False
-                                break
-                            elif value_id not in possible_values[cfield_id]:
-                                c = False
-                                break
-                            else:
-                                c_value = possible_values[cfield_id][value_id]
-                        
-                        cfield_value = field_values[cfield_id]
-                        if c_op == "==" and c_value not in cfield_value:
-                            c = False
-                            break
-                        elif c_op == "!=" and c_value in cfield_value:
-                            c = False
-                            break
-                    condition_satisfied |= c
-                    
-                if condition_satisfied:
-                    new_section.append([report_field, ", ".join(field_value)])
-                
-        new_section = [n for n in new_section if len(n[1]) > 0]
-        report_section.clear()
-        report_section += new_section
-
-
 
                 
 def unicoding(t):
