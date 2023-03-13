2a44ec435eac333e125c578d53a47c20a4eca7fd
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2023-03-13 10:24:11 +0100

    changed update/edit icon into pencil

--- scripts/migrate-form.py
+++ scripts/migrate-form.py
@@ -47,16 +47,13 @@ lipid_class_template_file = "../workflow-templates/lipid-class.json"
 entry_id_map = {}
 
 
-sql = "select DISTINCT entry_id  from TCrpQ_wpforms_entry_fields where form_id IN (199, 240, 245);"
-cursor_mysql.execute(sql)
+sql_prep = "select entry_id from TCrpQ_wpforms_entries where form_id IN (199, 240, 245) and status = 'partial'"
+cursor_mysql.execute("%s;" % sql_prep)
 len_entry_ids = len([row["entry_id"] for row in cursor_mysql.fetchall()])
 
 min_limit, num = 1, 1
 while min_limit < len_entry_ids:
-
-
-    
-    sql = "select entry_id, user_id, form_id, fields, date, status, user_uuid from TCrpQ_wpforms_entries where entry_id in (select DISTINCT entry_id  from TCrpQ_wpforms_entry_fields where form_id IN (199, 240, 245)) limit %i, 100;" % min_limit
+    sql = "select entry_id, user_id, form_id, fields, date, status, user_uuid from TCrpQ_wpforms_entries where entry_id in (%s) limit %i, 100;" % (sql_prep, min_limit)
     cursor_mysql.execute(sql)
     results = cursor_mysql.fetchall()
     
@@ -65,7 +62,7 @@ while min_limit < len_entry_ids:
         form_type = {245: "checklist", 240: "sample", 199: "lipid-class"}[result["form_id"]]
         uid = result["user_id"]
         entry_id = result["entry_id"]
-        #print("entry: %i, %s, %i of %i / %i" % (entry_id, form_type, num, len_entry_ids, min_limit))
+        print("entry: %i, %s, %i of %i / %i" % (entry_id, form_type, num, len_entry_ids, min_limit))
         num += 1
         
         if result["fields"] == None or len(result["fields"]) == 0: continue
@@ -118,7 +115,7 @@ while min_limit < len_entry_ids:
                     template_field["value"] = values
                     
 
-        sql = "insert into %sentries (form, user_id, status, fields, date, user_uuid) VALUES (?, ?, ?, ?, ?, ?, ?);" % table_prefix
+        sql = "insert into %sentries (form, user_id, status, fields, date, user_uuid) VALUES (?, ?, ?, ?, ?, ?);" % table_prefix
         cursor_sqlite.execute(sql, (form_id, uid, status, json.dumps(template_data), result["date"], result["user_uuid"]))
         conn_sqlite.commit()
         if entry_id == 3190: print(sql)
@@ -137,6 +134,105 @@ while min_limit < len_entry_ids:
             
         entry_id_map[entry_id] = new_entry_id
     min_limit += 100
+
+
+
+
+
+
+sql_prep = "select DISTINCT entry_id from TCrpQ_wpforms_entry_fields where form_id IN (199, 240, 245)"
+cursor_mysql.execute("%s;" % sql_prep)
+len_entry_ids = len([row["entry_id"] for row in cursor_mysql.fetchall()])
+
+min_limit, num = 1, 1
+while min_limit < len_entry_ids:
+    sql = "select entry_id, user_id, form_id, fields, date, status, user_uuid from TCrpQ_wpforms_entries where entry_id in (%s) limit %i, 100;" % (sql_prep, min_limit)
+    cursor_mysql.execute(sql)
+    results = cursor_mysql.fetchall()
+    
+    for result in results:
+
+        form_type = {245: "checklist", 240: "sample", 199: "lipid-class"}[result["form_id"]]
+        uid = result["user_id"]
+        entry_id = result["entry_id"]
+        print("entry: %i, %s, %i of %i / %i" % (entry_id, form_type, num, len_entry_ids, min_limit))
+        num += 1
+        
+        if result["fields"] == None or len(result["fields"]) == 0: continue
+        
+        if form_type == "checklist":
+            trans, template_data, form_id = cfn, json.loads(open(checklist_template_file, "rt").read()), main_form_id
+            
+        elif form_type == "sample":
+            trans, template_data, form_id = sfn, json.loads(open(sample_template_file, "rt").read()), sample_form_id
+            
+        elif form_type == "lipid-class":
+            trans, template_data, form_id = lcfn, json.loads(open(lipid_class_template_file, "rt").read()), class_form_id
+
+        instance_data = json.loads(result["fields"])
+
+        field_data = {}
+        for page in template_data["pages"]:
+            for field in page["content"]:
+                field_data[field["name"]] = field
+                
+        template_data["version"] = "v1.0.0"
+        template_data["current_page"] = 0
+        template_data["creation_date"] = str(result["date"])
+        status = result["status"]
+        
+        if entry_id == 3190: print(entry_id, status, form_type)
+        
+        if status == "": status = "completed"
+        elif status == "abandoned": status = "completed"
+        elif status not in {"partial", "permanent"}:
+            print("status error for entry_id %i" % entry_id)
+            continue
+
+        
+        for name, field in instance_data.items():
+            if "value" not in field: continue
+            
+            values = field["value"]
+            if len(values) == 0: continue
+            
+            if name in trans and trans[name] in field_data:
+                template_field = field_data[trans[name]]
+                if template_field["type"] in {"select", "multiple"}:
+                    values = set(values.split("\n"))
+                    for choice in template_field["choice"]:
+                        choice["value"] = 1 if choice["label"] in values else 0
+                            
+
+                else:
+                    template_field["value"] = values
+                    
+
+        sql = "insert into %sentries (form, user_id, status, fields, date, user_uuid) VALUES (?, ?, ?, ?, ?, ?);" % table_prefix
+        cursor_sqlite.execute(sql, (form_id, uid, status, json.dumps(template_data), result["date"], result["user_uuid"]))
+        conn_sqlite.commit()
+        if entry_id == 3190: print(sql)
+        
+        sql = "select max(id) as new_id from %sentries where user_id = ? and form = ?" % table_prefix
+        cursor_sqlite.execute(sql, (uid, form_id))
+        new_entry_id = cursor_sqlite.fetchone()["new_id"]
+
+        if form_type == "checklist":
+            hash_value = "%i-%i-%s" % (new_entry_id, uid, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            hash_value = hashlib.md5(hash_value.encode()).hexdigest()
+            
+            sql = "INSERT INTO %sreports (entry_id, hash) VALUES (?, ?);" % table_prefix
+            cursor_sqlite.execute(sql, (new_entry_id, hash_value))
+            conn_sqlite.commit()
+            
+        entry_id_map[entry_id] = new_entry_id
+    min_limit += 100
+
+
+
+
+
+
 
 
 sql = "select * from TCrpQ_wpforms_connect;"
