73216698e0deffe7eb74717ae5452564f1a58314
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2023-03-18 07:01:33 +0100

    new table view almost adopted completely

--- functions-sample.js
+++ functions-sample.js
@@ -7,12 +7,12 @@ function show_sample_selector(){
     update_load_sample_forms();
     
     window.addEventListener('resize', function(event) {
-        document.getElementById("sample_forms_table").style.height = String(document.getElementById("control-buttons-sample").clientHeight * 0.75) + "px";
+        document.getElementById("viewtable-import-sample").style.height = String(document.getElementById("control-buttons-sample").clientHeight * 0.75) + "px";
     }, true);
     
     document.getElementById("grey_background").style.display = "block";
     document.getElementById("sample_selector_wrapper").style.display = "block";
-    document.getElementById("sample_forms_table").style.height = String(document.getElementById("control-buttons-sample").clientHeight * 0.75) + "px";
+    document.getElementById("viewtable-import-sample").style.height = String(document.getElementById("control-buttons-sample").clientHeight * 0.75) + "px";
 }
 
 
@@ -20,43 +20,36 @@ function update_load_sample_forms(){
     refresh_sample_view();
     if (entry_id == undefined || entry_id.length == 0) return;
     var xmlhttp_request = new XMLHttpRequest();
-    document.getElementById("sample_forms_table").innerHTML = "";
     
     xmlhttp_request.onreadystatechange = function() {
         if (xmlhttp_request.readyState == 4 && xmlhttp_request.status == 200) {
             response_text = xmlhttp_request.responseText;
-            var innerHTML = "";
-            var post = 1;
             if (response_text.length > 0){
+                document.getElementById("viewtable-import-sample").resetTable();
                 if (!response_text.startsWith("ErrorCodes")){
                     var response = JSON.parse(response_text);
-                    innerHTML += "<table cellspacing='0' cellpadding='10' style='width: 100%'>";
-                    innerHTML += "<tr><th style='width: 3%; border-bottom: 3px solid black;'>&nbsp;</th>";
-                    innerHTML += "<th style='width: 90%; border-bottom: 3px solid black;'>Sample</th>";
-                    innerHTML += "<th style='border-bottom: 3px solid black;'>Selection</th></tr>";
                     
-                    response.sort(function(a, b){
-                        return a["title"] > b["title"];
-                    });
                     
                     for (var i = 0; i < response.length; ++i){
                         var row = response[i];
-                        if (!("title" in row)){
+                        if (!("title" in row) || !("entry_id" in row)){
                             echo("Error");
                             return;
                         }
                         
-                        innerHTML += "<tr><td>" + String(post++) + ") </td>";
-                        innerHTML += "<td>" + row["title"] + "</td>";
-                        innerHTML += "<td><input type='checkbox' id='" + row["entry_id"] + "' class='check_sample'></input></td>";
-                        innerHTML += "</tr>";
+                        var table_row = [];
+                        table_row.push([row["title"] + " "]);
+                        
+                        var checkbox_obj = document.createElement("input");
+                        checkbox_obj.type = "checkbox";
+                        checkbox_obj.setAttribute("class", "check_sample");
+                        checkbox_obj.setAttribute("id", row["entry_id"]);
+                        table_row.push([checkbox_obj]);
+                        
+                        document.getElementById("viewtable-import-sample").addRow(table_row);
                     }
-                    
-                    
-                    innerHTML += "</table>";
                 }
             }
-            document.getElementById("sample_forms_table").innerHTML = innerHTML;
         }
     }
     var request_url = connector_path + "/connector.php?command=get_all_sample_forms";
@@ -178,7 +171,6 @@ function update_sample_forms() {
                 
                     if (sample_field_object != null){
                         if (!("value" in sample_field_object)) sample_field_object["value"] = 0;
-                        
                         sample_field_object["value"] = !has_partial_samples;
                     }
                 }
