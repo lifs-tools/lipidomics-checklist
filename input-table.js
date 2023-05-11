1e22b614f84b49e0d731118b4d88782554ccb447
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2023-05-11 12:52:58 +0200

    suggestions almost inserted

--- input-table.js
+++ input-table.js
@@ -39,6 +39,7 @@ class InputTable extends HTMLElement {
     addSuggestions(index, suggestion_list){
         if (0 <= index && index < this.suggestions.length){
             this.suggestions[index] = suggestion_list;
+            this.renderTable();
         }
     }
     
@@ -72,21 +73,10 @@ class InputTable extends HTMLElement {
         this.obj.updateTable();
     }
     
-    
-    showSuggestions(obj, cell){
-        /*
-        var dialog = document.createElement("display");
-        dialog.style = "border: 1px solid black; border-radius: 5px;";
-        dialog.innerHTML = "huhu";
-        obj.appendChild(dialog);
-        dialog.showModal();
-        //console.log(cell, obj.getBoundingClientRect().left);
-        */
-        
-        
-        //console.log(cell, obj.getBoundingClientRect().left);
-        //console.log(inner);
-        
+    updateTextObj(obj, field){
+        console.log(obj);
+        obj.data[field.row_num][field.cell_num] = encodeURIComponent(field.value);
+        obj.updateTable();
     }
     
     
@@ -123,10 +113,12 @@ class InputTable extends HTMLElement {
                 input_obj.obj = this;
                 input_obj.row_num = row_num;
                 input_obj.cell_num = cell_num;
+                input_obj.onchange = this.updateText;
+                input_obj.style = "padding: 2px 0 2px 0; border: 1px solid #ccc; border-radius: 2px; width: 98%";
                 if (this.suggestions[cell_num] != 0){
                     var suggestion_field = document.createElement("ul");
                     suggestion_field.setAttribute("class", "suggestion-field");
-                    suggestion_field.style = "position: absolute; left: " + (div_obj.getBoundingClientRect().left + div_obj.getBoundingClientRect().width - 10) + "px; top: " + div_obj.getBoundingClientRect().top + "px";
+                    suggestion_field.style = "position: absolute; left: " + (div_obj.getBoundingClientRect().left + div_obj.getBoundingClientRect().width - 10) + "px;"
                     div_obj.appendChild(suggestion_field);
                     
                     for (var suggestion of this.suggestions[cell_num]){
@@ -134,15 +126,14 @@ class InputTable extends HTMLElement {
                         suggestion_field.appendChild(suggestion_cell);
                         suggestion_cell.innerHTML = suggestion;
                         suggestion_cell.ref = input_obj;
+                        suggestion_cell.inputTable = this;
                         suggestion_cell.setAttribute("class", "suggestion-cell");
-                        suggestion_cell.setAttribute("onclick", "this.ref.value = this.innerHTML;");
+                        suggestion_cell.setAttribute("onclick", "this.ref.value = this.innerHTML; this.inputTable.updateTextObj(this.inputTable, this.ref);");
                     }
                     
                     var cell_number = cell_num;
                     var div = div_obj;
                 }
-                input_obj.onchange = this.updateText;
-                input_obj.style = "padding: 2px 0 2px 0; border: 1px solid #ccc; border-radius: 2px; width: 98%";
                 cell_num++;
             }
             var td_obj_del = document.createElement("td");
